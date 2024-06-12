519f8389d9bd1591a3552e192593fdea2186f0b3
Stanton Collyer <admin@sphereframe.com.au>
2024-06-13 00:25:59 +1000

    Add UI for splitting up AOVs on import

--- src/iom4m_read.py
+++ src/iom4m_read.py
@@ -1,4 +1,25 @@
 from PySide2 import QtCore, QtGui, QtWidgets
+from PySide2.QtCore import QModelIndex
+import nuke
+
+class AovListModel(QtGui.QStandardItemModel):
+
+    def __init__(self):
+
+        super().__init__()
+        self.aovList = []
+        self.root = self.invisibleRootItem()
+    
+    def appendRow(self, item: QtGui.QStandardItem):
+
+        self.root.appendRow(item)
+        self.aovList.append(item)
+        print(self.aovList)
+        
+    def removeRow(self, row: int):
+        
+        self.root.removeRow(row)
+        self.aovList.pop(row)
 
 class Window(QtWidgets.QWidget):
 
@@ -8,6 +29,10 @@ class Window(QtWidgets.QWidget):
         self.setWindowFlags(QtGui.Qt.WindowStaysOnTopHint)
         self.resize(300, 100)
         self.setWindowTitle("Read Media (IOMatrix)")
+        
+        self.font = QtGui.QFont()
+        self.font.setPointSize(12)
+        self.setFont(self.font)
         
         self.layout = QtWidgets.QVBoxLayout(self)
         
@@ -27,28 +52,86 @@ class Window(QtWidgets.QWidget):
 
         self.colSpcBox = QtWidgets.QComboBox(self)
         self.layout.addWidget(self.colSpcBox)
-        
-        self.invertBox = QtWidgets.QCheckBox("Invert colour transform", self)
-        self.layout.addWidget(self.invertBox)        
-
         for cs in nuke.getOcioColorSpaces():
 
             self.colSpcBox.addItem(cs.split("\t")[0])
         
+        self.invertBox = QtWidgets.QCheckBox("Invert colour transform", self)
+        self.layout.addWidget(self.invertBox)        
+        
+        self.aovL = QtWidgets.QLabel("AOVs", self)
+        self.layout.addWidget(self.aovL)
+        
+        self.aovBox = QtWidgets.QListView(self)
+        self.aovBoxModel = AovListModel()
+        self.aovBoxSel = QtCore.QItemSelectionModel(self.aovBoxModel)
+        
+        self.aovBox.setModel(self.aovBoxModel)
+        self.aovBox.setSelectionModel(self.aovBoxSel)
+        self.aovBox.setSelectionMode(QtWidgets.QAbstractItemView.ExtendedSelection)
+        self.aovBox.setMinimumHeight(300)
+        self.layout.addWidget(self.aovBox)
+        
+        self.aovBtnsLayout = QtWidgets.QHBoxLayout(self)
+        
+        self.aovAddBtn = QtWidgets.QPushButton("Add", self)
+        self.aovAddBtn.clicked.connect(self.addAov)
+        
+        self.aovTypeBox = QtWidgets.QComboBox(self)
+
+        self.aovRmBtn = QtWidgets.QPushButton("Remove", self)
+        self.aovRmBtn.clicked.connect(self.rmAov)
+
+        self.aovBtnsLayout.addWidget(self.aovAddBtn)
+        self.aovBtnsLayout.addWidget(self.aovTypeBox)
+        self.aovBtnsLayout.addWidget(self.aovRmBtn)
+        self.layout.addLayout(self.aovBtnsLayout)
+
         self.button = QtWidgets.QPushButton("Create Read", self)
         self.button.clicked.connect(self.createRead)
         self.layout.addWidget(self.button)
         
         self.layout.addStretch()
 
+    def addAov(self):
+        
+        if self.aovTypeBox.count() > 0:
+            
+            self.aovBoxModel.appendRow(QtGui.QStandardItem(self.aovTypeBox.currentText()))
+    
+    def rmAov(self):
+        
+        selection = self.aovBoxSel.selectedIndexes()
+        removeList = []
+        for index in selection:
+            
+            removeList.append(self.aovBoxModel.itemFromIndex(index))
+            
+        for item in removeList:
+            
+            self.aovBoxModel.removeRow(item.row())
+        
+        
     def chooseFile(self):
         
         self.pathField.setText(QtWidgets.QFileDialog.getOpenFileName()[0])
+        
+        readTmp = nuke.nodes.Read(file=self.pathField.text(), colorspace="Raw")
+        
+        self.aovTypeBox.clear()
+        layers = nuke.layers(readTmp)
+        for layer in layers:
+            
+            self.aovTypeBox.addItem(layer)
+
+        nuke.delete(readTmp)
 
     def createRead(self):
         
         viewer = nuke.activeViewer().node()
         
+        finalTransform = 0
+
         if self.invertBox.isChecked():
             
             process = nuke.ViewerProcess.node(viewer["viewerProcess"].value())
@@ -56,11 +139,17 @@ class Window(QtWidgets.QWidget):
             read = nuke.nodes.Read(file=self.pathField.text(), colorspace="Raw")
             ocio = nuke.nodes.OCIODisplay(colorspace=self.colSpcBox.currentText(), invert=True, display=process["display"].value(), view=process["view"].value())
             ocio.setInput(0, read)
-            viewer.setInput(0, ocio)
-            return
-
-        read = nuke.nodes.Read(file=self.pathField.text(), colorspace=self.colSpcBox.currentText())
-        viewer.setInput(0, read)
+            finalTransform = ocio
+        
+        else:
+            
+            read = nuke.nodes.Read(file=self.pathField.text(), colorspace=self.colSpcBox.currentText())
+            finalTransform = read
+            
+        for aov in self.aovBoxModel.aovList:
+            
+            shuffle = nuke.nodes.Shuffle2(in1 = aov.text())
+            shuffle.setInput(0, finalTransform)
 
 window = Window()
 window.show()
